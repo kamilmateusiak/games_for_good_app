4580468447aa5e72e0a58a15a9b7b129382c70fb
kamilmateusiak <kamil.mateusiak@swing.dev>
2022-11-19 23:54:35 +0100

    remove gap

--- components/Menu/style.ts
+++ components/Menu/style.ts
@@ -4,11 +4,10 @@ export const Wrapper = styled.div`
   display: flex;
   width: fit-content;
   flex-direction: row;
-  gap: 50px;
   margin: 20px 0;
   width: 100%;
   justify-content: space-between;
-  padding: 0 20px;
+  padding: 0 40px;
 `;
 
 export const IconContainer = styled.div`
