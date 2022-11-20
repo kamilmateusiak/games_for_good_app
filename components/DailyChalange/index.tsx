1558692b157e1b5938eb059c79bd77cdc7910e4c
natimati <natmateusiak@gmail.com>
2022-11-20 09:47:56 +0100

    adding chdaily challange body, adding border to menu

--- components/DailyChalange/index.tsx
+++ components/DailyChalange/index.tsx
@@ -1,14 +1,35 @@
 import Image from "next/image";
-import { ChallengeCategory, ChallengeImage, Wrapper } from "./style";
+import Link from "next/link";
+import { Label } from "../../style";
+import { AdvanceBar, ChallageContainer, ChallengeAdvance, ChallengeAdvanceContainer, ChallengeCategory, ChallengeImage, TitleHeader, Wrapper } from "./style";
 
 export default function DailyChalange() {
   return (
-    <Wrapper>
-      <h2>Daily chalange</h2>
-      <ChallengeImage height='261' width='316' src={"/assets/daily_challange_image.png"} alt={"daily challange"} />
-      <ChallengeCategory>
-        <h3>Plant</h3>
-      </ChallengeCategory>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <h2>Daily chalange</h2>
+        <ChallengeImage height='261' width='316' src={"/assets/daily_challange_image.png"} alt={"daily challange"} />
+        <ChallengeCategory>
+          <h4>Plant</h4>
+        </ChallengeCategory>
+      </Wrapper>
+      <TitleHeader>
+        <h3>You have to do</h3>
+        <Link href={""}>
+          <Label>view all</Label>
+        </Link>
+      </TitleHeader>
+      <ChallageContainer>
+        <h4>Plant exercise</h4>
+        <p>Plant exercise<br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita tempora possimus nostrum nesciunt aliquid distinctio hic quasi ab assumenda incidunt, tempore fugit in amet consequatur qui unde velit quo similique?</p>
+        <ChallengeAdvanceContainer>
+          <ChallengeAdvance>
+            <AdvanceBar />
+          </ChallengeAdvance>
+          <p>95%</p>
+        </ChallengeAdvanceContainer>
+      </ChallageContainer>
+    </>
   )
 };
--- components/Menu/style.ts
+++ components/Menu/style.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  margin: 5px 10px;
+  border-top: 1px solid black;
+`;
+
 export const Wrapper = styled.div`
   display: flex;
   width: fit-content;
@@ -7,8 +12,8 @@ export const Wrapper = styled.div`
   margin: 20px 0;
   width: 100%;
   justify-content: space-between;
-  padding: 0 40px;
-`;
+  padding: 0px 30px;
+  `;
 
 export const IconContainer = styled.div`
   display: flex;
