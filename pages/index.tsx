25b5bec033a77c978310429acc4f1ea773dee940
natimati <natmateusiak@gmail.com>
2022-11-20 08:15:32 +0100

    creating home header ang daily challange header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import DailyChalange from "../components/DailyChalange";
+import HomeHeader from "../components/HomeHeader";
+
 export default function Home() {
   return (
     <div>
-      Games for good main page
+      <HomeHeader />
+      <DailyChalange />
     </div>
   )
 }
